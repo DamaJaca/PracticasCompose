6dfea1a1b5bd2adbc6e41c3740c61754f2a1f16b
DamaJaca <danielcardenete1994@gmail.com>
2024-05-28 12:46:54 +0200

    RememberScreen funcional

--- app/src/main/java/com/djcdev/practicascompose/ui/screens/remember/RememberScreen.kt
+++ app/src/main/java/com/djcdev/practicascompose/ui/screens/remember/RememberScreen.kt
@@ -1,5 +1,6 @@
 package com.djcdev.practicascompose.ui.screens.remember
 
+import android.widget.Toast
 import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Arrangement
@@ -19,21 +20,25 @@ import androidx.compose.material3.OutlinedButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
+import androidx.hilt.navigation.compose.hiltViewModel
 import androidx.navigation.NavController
 import androidx.navigation.compose.rememberNavController
+import com.djcdev.practicascompose.domain.model.exceptions.FailedLogin
 import com.djcdev.practicascompose.ui.companioncomposables.ComposeStructure
 import com.djcdev.practicascompose.ui.companioncomposables.HeadderIconIberdrola
-import com.djcdev.practicascompose.ui.navigation.Home
 import com.djcdev.practicascompose.ui.navigation.Login
 import com.djcdev.practicascompose.ui.screens.login.RelativeLayoutComponent
 
@@ -80,29 +85,50 @@ fun ContentRemember(navController: NavController) {
 @Composable
 fun BottomRemember(navController: NavController, modifier: Modifier = Modifier) {
 
+    val viewModel: RememberViewModel = hiltViewModel()
+
+    val context = LocalContext.current
+
+    val isLoading by viewModel.isLoading.collectAsState()
+    val email by viewModel.email.collectAsState()
+    var showDialog by rememberSaveable {
+        mutableStateOf(false)
+    }
+    var failedLog: FailedLogin? by remember {
+        mutableStateOf(null)
+    }
+    var logged by rememberSaveable {
+        mutableStateOf(false)
+    }
 
 
-    var isLoading by remember { mutableStateOf(false) }
     Column (
         modifier = modifier
     ) {
+        if (logged) Toast.makeText(
+            LocalContext.current,
+            "Se ha enviado el correo de recuperacion",
+            Toast.LENGTH_SHORT
+        ).show()
         Button(
             onClick = {
-                //                        viewModel.login(username, password) { success: Boolean, fail: FailedLogin ->
-                //                            isLoading = false
-                //                            if (success) {
-                //                                Toast.makeText(context, "Se ha iniciado sesion", Toast.LENGTH_SHORT)
-                //                                    .show()
-                //                                if (rememberPassword) {
-                //                                    // Save username and password
-                //                                }
-                //                                navController.navigate("home")
-                //                            } else {
-                //                                // Handle login failure
-                //                                Toast.makeText(context, "Login failed", Toast.LENGTH_SHORT).show()
-                //                            }
-                //
-                navController.navigate(Home)
+                viewModel.changeIsLoading(true)
+                if(email!=""){
+                    viewModel.remember(email) { success: Boolean, fail: FailedLogin? ->
+                        if (fail == null) {
+                            if (success) {
+                                logged = true
+                            }
+                            navController.navigate(Login)
+                        } else {
+                            failedLog = fail
+                            showDialog = true
+                        }
+                    }
+                }else{
+                    failedLog=FailedLogin.MissingSomething
+                    showDialog=true
+                }
             },
             colors = ButtonDefaults.buttonColors(
                 containerColor = MaterialTheme.colorScheme.primary
---
+++ app/src/main/java/com/djcdev/practicascompose/ui/screens/remember/RememberViewModel.kt
@@ -0,0 +1,39 @@
+package com.djcdev.practicascompose.ui.screens.remember
+
+import androidx.lifecycle.ViewModel
+import com.djcdev.practicas.domain.usecase.LoginUseCase
+import com.djcdev.practicas.domain.usecase.RememberUserUseCase
+import com.djcdev.practicas.domain.usecase.SingUpUseCase
+import com.djcdev.practicascompose.domain.model.exceptions.FailedLogin
+import com.djcdev.practicascompose.domain.model.exceptions.FailedSignUp
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import javax.inject.Inject
+
+@HiltViewModel
+class RememberViewModel @Inject constructor(
+    private val rememberUserUseCase: RememberUserUseCase
+): ViewModel(){
+
+    private val _email = MutableStateFlow("")
+    val email get () = _email
+    fun setEmail(email:String){
+        _email.value=email
+    }
+
+
+
+    private var _isLoading = MutableStateFlow(false)
+    val isLoading get () = _isLoading
+
+    fun changeIsLoading(boolean: Boolean){
+        _isLoading.value= boolean
+    }
+
+
+
+    fun remember(user: String, remember: (Boolean, FailedLogin?) -> Unit) {
+        rememberUserUseCase(user) { boolean, failed -> remember(boolean, failed) }
+    }
+
+}
